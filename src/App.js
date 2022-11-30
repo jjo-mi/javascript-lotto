7f2e6bde3a6956286b9c8acec417fb0578ded28d
jjo-mi <whdal2636@naver.com>
2022-11-30 16:33:07 +0900

    feat: 1. 로또 구입금액 입력 및 유효성검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const { Console,Random } = require("@woowacourse/mission-utils");
+const { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE } = require("./Constant");
+
+
 class App {
-  play() {}
+  play() {
+    this.inputPurchaseAmount();
+
+  }
+
+  /** 1. 로또 구입금액 받기 */
+  inputPurchaseAmount() {
+    Console.readLine(INPUT_MESSAGE.PURCHASE, (input) => {
+      let ticket = input / 1000;
+      console.log(ticket);
+      this.validatePurchase(input);
+
+      
+    });
+  
+  }
+
+  /** 1-1. 입력값 유효성검사 */
+  validatePurchase(input) {
+    if (input % 1000 !== 0 || isNaN(input)) {
+      throw new Error(ERROR_MESSAGE.PURCHASE);
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
