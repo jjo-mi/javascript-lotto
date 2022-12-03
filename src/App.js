ce348226c04177c30accd0fe54c5d7e0ec407ed7
jjo-mi <whdal2636@naver.com>
2022-12-03 16:12:00 +0900

    feat: 2~3 구매금액만큼 로또 생성 후 출력력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 const { Console,Random } = require("@woowacourse/mission-utils");
 const { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE } = require("./Constant");
+const Lotto = require("./Lotto");
 
 
 class App {
+  // ticket;
+
+  constructor() {
+    this.ticket = 0;
+    this.lottoArr = [];
+  }
+
   play() {
     this.inputPurchaseAmount();
 
@@ -11,13 +19,12 @@ class App {
   /** 1. 로또 구입금액 받기 */
   inputPurchaseAmount() {
     Console.readLine(INPUT_MESSAGE.PURCHASE, (input) => {
-      let ticket = input / 1000;
-      console.log(ticket);
       this.validatePurchase(input);
-
-      
+      this.ticket = input / 1000;
+      console.log(this.ticket);
+      this.printPurchaseAmount();
+      this.creatLottos();
     });
-  
   }
 
   /** 1-1. 입력값 유효성검사 */
@@ -26,7 +33,20 @@ class App {
       throw new Error(ERROR_MESSAGE.PURCHASE);
     }
   }
-}
+
+  /** 2. 구매금액만큼 로또 번호 생성하기 */
+  creatLottos() {
+    for(let i = 0; i < this.ticket; i++) {
+      let numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      let lotto = new Lotto(numbers);
+      this.lottoArr.push(lotto);
+    }
+  }
+
+  printPurchaseAmount() {
+    Console.print(this.ticket + OUTPUT_MESSAGE.PURCHASE_AMOUNT);
+  }
+} 
 
 const app = new App();
 app.play();
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,16 +1,47 @@
 class Lotto {
   #numbers;
-
+ 
   constructor(numbers) {
     this.validate(numbers);
+    this.sortNumbers(numbers);
     this.#numbers = numbers;
+    this.print();
+  }
+
+  get numbers() {
+    return this.#numbers;
   }
 
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    } 
   }
+
+  sortNumbers(numbers) {
+    return numbers.sort((a, b) => a-b);
+  }
+
+  print() {
+    console.log(this.#numbers);
+  }
+
+
+  // /** 2. 로또 구매금액만큼 로또티켓 생성하기 */
+  // creatLotto(ticket) {
+  //   let totalLottos = [];
+  //   const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+
+  //   for(let i = 0; i < ticket; i++) {
+  //     this.validate(numbers);
+  //     totalLottos.push(numbers);  
+  //   }
+  //   totalLottos.forEach(el => {
+  //     console.log(el)
+  //   });
+  //   return totalLottos;
+  // }
+
 
   // TODO: 추가 기능 구현
 }
